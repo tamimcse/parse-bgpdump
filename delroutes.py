328f00d9a407db2b71eebfac84bc3fe2ed13091f
tamimcse <tamim@csebuet.org>
2018-11-24 17:44:10 -0500

    .

--- delroutes.py
+++ delroutes.py
@@ -1,6 +1,6 @@
 from subprocess import call
 
-with open("Routes/routes-293") as f:
+with open("Routes/routes-R1") as f:
     for line in f:
         # Do something with 'line'
 	arr = line.replace("\r\n", "").split("\t")
