a27a84b4607f6f36cf18173460ecba403f0d90b8
tamimcse <tamim@csebuet.org>
2019-04-17 19:04:20 -0400

    .

--- calc_mem_ip6.py
+++ calc_mem_ip6.py
@@ -1,13 +1,20 @@
+from socket import inet_pton, AF_INET6
+from struct import unpack
 import socket
 import struct
 
 def ip2int(addr):
     return struct.unpack("!I", socket.inet_aton(addr))[0]
 
-ip_arr_24 = {}
+def ip6_to_integer(ip6):
+    ip6 = inet_pton(AF_INET6, ip6)
+    a, b = unpack(">QQ", ip6)
+    return (a << 64) | b
+
 ip_arr_32 = {}
-ck24_count = 0
+ip_arr_48 = {}
 ck32_count = 0
+ck48_count = 0
 saill = 0.0
 sailbm = 0.0
 
@@ -16,25 +23,26 @@ with open("ipv6-fibs/routes-293") as f:
         # Do something with 'line'
 	arr = line.replace("\r\n", "").split("\t")
 	prefix = arr[0].split("/")
-	if int(prefix[1]) >= 17 and int(prefix[1]) <= 24 :
-          ip = ip2int(prefix[0]) >> 16
-          if ip not in ip_arr_24 :
-	    ck24_count += 1
-	    ip_arr_24[ip] = True	                  
-	elif  int(prefix[1]) > 24 :
-          ip = ip2int(prefix[0]) >> 8
+
+	if int(prefix[1]) >= 17 and int(prefix[1]) <= 32 :
+          ip = ip6_to_integer(prefix[0]) >> 48
           if ip not in ip_arr_32 :
 	    ck32_count += 1
 	    ip_arr_32[ip] = True	                  
-    print "ck24_count=", ck24_count
+	elif  int(prefix[1]) >= 33 and int(prefix[1]) <= 48 :
+          ip = (ip6_to_integer(prefix[0]) & 0XFFFF00000000) >> 32
+          if ip not in ip_arr_48 :
+	    ck48_count += 1
+	    ip_arr_48[ip] = True	                  
     print "ck32_count=", ck32_count
+    print "ck48_count=", ck48_count
 
-    if ck32_count > 0:
-    	saill = (64 + 128 + ((ck24_count * 256)/1024) + ((ck24_count * 4 * 256)/1024) + ((ck32_count * 256)/1024))/1024.0
+    if ck48_count > 0:
+    	saill = (64 + 128 + ((ck32_count * 256)/1024) + ((ck32_count * 4 * 256)/1024) + ((ck48_count * 256)/1024))/1024.0
     else:
-    	saill = (64 + 128 + ((ck24_count * 256)/1024))/1024.0
+    	saill = (64 + 128 + ((ck32_count * 256)/1024))/1024.0
 
-    sailbm = (64 + 128 + ((ck24_count * 256)/1024) + ((ck32_count * 68)/1024) + ((ck32_count * 256)/1024))/1024.0
+    sailbm = (64 + 128 + ((ck32_count * 256)/1024) + ((ck48_count * 68)/1024) + ((ck48_count * 256)/1024))/1024.0
 
     print "sail-l=", saill
     print "sail-bm=", sailbm
