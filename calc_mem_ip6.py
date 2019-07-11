7f3d1e7d3ba7008e8f079fc7429d92fa40db7e26
tamimcse <tamim@csebuet.org>
2019-07-10 20:41:43 -0400

    Calculated the number of Ns accurately

--- calc_mem_ip6.py
+++ calc_mem_ip6.py
@@ -23,6 +23,10 @@ ck40_count = 0
 ck48_count = 0
 ck56_count = 0
 ck64_count = 0
+n32_cnt = 0
+n40_cnt = 0
+n48_cnt = 0
+n64_cnt = 0
 ppc = 0
 sail = 0
 
@@ -42,12 +46,14 @@ with open(filename) as f:
           if ip not in ip_arr_32 :
 	    ck32_count += 1
 	    ip_arr_32[ip] = True
+            n32_cnt += 1 << (32 - int(prefix[1]))
 
 	elif int(prefix[1]) > 32 and int(prefix[1]) <= 40 :
           ip = prefix64 >> 32
           if ip not in ip_arr_40 :
 	    ck40_count += 1
 	    ip_arr_40[ip] = True
+            n40_cnt += 1 << (40 - int(prefix[1]))
 
           ip = prefix64 >> 48
           if ip not in ip_arr_32 :
@@ -59,6 +65,7 @@ with open(filename) as f:
           if ip not in ip_arr_48 :
 	    ck48_count += 1
 	    ip_arr_48[ip] = True
+            n48_cnt += 1 << (48 - int(prefix[1]))
 
           ip = prefix64 >> 32
           if ip not in ip_arr_40 :
@@ -75,6 +82,7 @@ with open(filename) as f:
           if ip not in ip_arr_64 :
 	    ck64_count += 1
 	    ip_arr_64[ip] = True
+            n64_cnt += 1 << (64 - int(prefix[1]))
 
           ip = prefix64 >> 24
           if ip not in ip_arr_48 :
@@ -95,9 +103,12 @@ with open(filename) as f:
     print "ck40_count=", ck40_count
     print "ck48_count=", ck48_count
     print "ck64_count=", ck64_count
+    print "n32_cnt=", n32_cnt
+    print "n40_cnt=", n40_cnt
+    print "n48_cnt=", n48_cnt
+    print "n64_cnt=", n64_cnt
 
-#Assume that Level 32 and 64 have 600 Ns
-    ppc = (65536 * 3 + 600 + (ck40_count + ck48_count) * 16 + ck32_count * 20 * 1024 + + ck64_count * 10 * 1024 + (ck40_count + ck48_count) * 10 * 4)/(1024*1024)
+    ppc = (65536 * 3 + n32_cnt + n40_cnt + n48_cnt + n64_cnt + ck32_count * 20 * 1024 + + ck64_count * 10 * 1024 + (ck40_count + ck48_count) * 10 * 4)/(1024*1024)
     sail = (65536 * 3 + (ck32_count + ck64_count) * 65536 + (ck40_count + ck48_count) * 256 + ck32_count * 65536 * 4 + (ck40_count + ck48_count) * 256 * 4)/(1024*1024)
     print "PPC=", ppc , "MB"
     print "SAIL-16-32-40-48-64 =", sail, "MB"
