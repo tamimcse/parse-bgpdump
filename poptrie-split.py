b06f888ad7897cc6579750c673bd32d687e8f3c2
tamimcse <tamim@csebuet.org>
2019-09-10 21:51:54 -0400

    .

--- poptrie-split.py
+++ poptrie-split.py
@@ -16,24 +16,32 @@ filenames = ["routes-293", "routes-852", "routes-19016", "routes-19151", "routes
 
 for filename in filenames :
 
-	ip_arr_24 = {}
-	ip_arr_32 = {}
+	ip_arr_22 = {}
+	ip_arr_28 = {}
+	ip_arr_34 = {}
 	ip_arr_40 = {}
-	ip_arr_48 = {}
-	ip_arr_56 = {}
+	ip_arr_46 = {}
+	ip_arr_52 = {}
+	ip_arr_58 = {}
 	ip_arr_64 = {}
-	ck24_count = 0
-	ck32_count = 0
+	ck22_count = 0
+	ck28_count = 0
+	ck34_count = 0
 	ck40_count = 0
-	ck48_count = 0
-	ck56_count = 0
+	ck46_count = 0
+	ck52_count = 0
+	ck58_count = 0
 	ck64_count = 0
-	n32_cnt = 0
+	n22_cnt = 0
+	n28_cnt = 0
+	n34_cnt = 0
 	n40_cnt = 0
-	n48_cnt = 0
+	n46_cnt = 0
+	n52_cnt = 0
+	n58_cnt = 0
 	n64_cnt = 0
-	ppc = 0
-	sail = 0
+
+	poptrie = 0
   
 	with open("ipv6-fibs/" + filename) as f:
 	    for line in f:
@@ -42,63 +50,202 @@ for filename in filenames :
 		prefix = arr[0].split("/")
 	#       Extract 64-bit from the 128-bit prefix
 		prefix64 = ip6_to_integer(prefix[0]) >> 64
-		if int(prefix[1]) > 16 and int(prefix[1]) <= 32 :
-		  ip = prefix64 >> 48
-		  if ip not in ip_arr_32 :
-		    ck32_count += 1
-		    ip_arr_32[ip] = True
-		  n32_cnt += 1 << (32 - int(prefix[1]))
 
-		elif int(prefix[1]) > 32 and int(prefix[1]) <= 40 :
-		  ip = prefix64 >> 32
+		if int(prefix[1]) > 16 and int(prefix[1]) <= 22 :
+		  ip = prefix64 >> 48
+		  if ip not in ip_arr_22 :
+		    ck22_count += 1
+		    ip_arr_22[ip] = True
+		  n22_cnt += 1 << (22 - int(prefix[1]))
+
+		if int(prefix[1]) > 22 and int(prefix[1]) <= 28 :
+		  ip = prefix64 >> 42
+		  if ip not in ip_arr_28 :
+		    ck28_count += 1
+		    ip_arr_28[ip] = True
+		  n28_cnt += 1 << (28 - int(prefix[1]))
+
+		  ip = prefix64 >> 48
+		  if ip not in ip_arr_22 :
+		    ck22_count += 1
+		    ip_arr_22[ip] = True
+
+		elif int(prefix[1]) > 28 and int(prefix[1]) <= 34 :
+		  ip = prefix64 >> 36
+		  if ip not in ip_arr_34 :
+		    ck34_count += 1
+		    ip_arr_34[ip] = True
+		  n34_cnt += 1 << (34 - int(prefix[1]))
+
+		  ip = prefix64 >> 42
+		  if ip not in ip_arr_28 :
+		    ck28_count += 1
+		    ip_arr_28[ip] = True
+
+		  ip = prefix64 >> 48
+		  if ip not in ip_arr_22 :
+		    ck22_count += 1
+		    ip_arr_22[ip] = True
+
+		elif int(prefix[1]) > 34 and int(prefix[1]) <= 40 :
+		  ip = prefix64 >> 30
 		  if ip not in ip_arr_40 :
 		    ck40_count += 1
 		    ip_arr_40[ip] = True
 		  n40_cnt += 1 << (40 - int(prefix[1]))
 
+		  ip = prefix64 >> 36
+		  if ip not in ip_arr_34 :
+		    ck34_count += 1
+		    ip_arr_34[ip] = True
+
+		  ip = prefix64 >> 42
+		  if ip not in ip_arr_28 :
+		    ck28_count += 1
+		    ip_arr_28[ip] = True
+
 		  ip = prefix64 >> 48
-		  if ip not in ip_arr_32 :
-		    ck32_count += 1
-		    ip_arr_32[ip] = True
+		  if ip not in ip_arr_22 :
+		    ck22_count += 1
+		    ip_arr_22[ip] = True
 
-		elif int(prefix[1]) > 40 and int(prefix[1]) <= 48 :
+		elif int(prefix[1]) > 40 and int(prefix[1]) <= 46 :
 		  ip = prefix64 >> 24
-		  if ip not in ip_arr_48 :
-		    ck48_count += 1
-		    ip_arr_48[ip] = True
-		  n48_cnt += 1 << (48 - int(prefix[1]))
+		  if ip not in ip_arr_46 :
+		    ck46_count += 1
+		    ip_arr_46[ip] = True
+		  n46_cnt += 1 << (46 - int(prefix[1]))
 
-		  ip = prefix64 >> 32
+		  ip = prefix64 >> 30
 		  if ip not in ip_arr_40 :
 		    ck40_count += 1
 		    ip_arr_40[ip] = True
 
-		  ip = prefix64 >> 48
-		  if ip not in ip_arr_32 :
-		    ck32_count += 1
-		    ip_arr_32[ip] = True
+		  ip = prefix64 >> 36
+		  if ip not in ip_arr_34 :
+		    ck34_count += 1
+		    ip_arr_34[ip] = True
 
-		elif  int(prefix[1]) > 48 and int(prefix[1]) <= 64 :
-		  ip = prefix64 >> 16
+		  ip = prefix64 >> 42
+		  if ip not in ip_arr_28 :
+		    ck28_count += 1
+		    ip_arr_28[ip] = True
+
+		  ip = prefix64 >> 48
+		  if ip not in ip_arr_22 :
+		    ck22_count += 1
+		    ip_arr_22[ip] = True
+
+		elif int(prefix[1]) > 46 and int(prefix[1]) <= 52 :
+		  ip = prefix64 >> 18
+		  if ip not in ip_arr_52 :
+		    ck52_count += 1
+		    ip_arr_52[ip] = True
+		  n52_cnt += 1 << (52 - int(prefix[1]))
+
+		  ip = prefix64 >> 24
+		  if ip not in ip_arr_46 :
+		    ck46_count += 1
+		    ip_arr_46[ip] = True
+
+		  ip = prefix64 >> 30
+		  if ip not in ip_arr_40 :
+		    ck40_count += 1
+		    ip_arr_40[ip] = True
+
+		  ip = prefix64 >> 36
+		  if ip not in ip_arr_34 :
+		    ck34_count += 1
+		    ip_arr_34[ip] = True
+
+		  ip = prefix64 >> 42
+		  if ip not in ip_arr_28 :
+		    ck28_count += 1
+		    ip_arr_28[ip] = True
+
+		  ip = prefix64 >> 48
+		  if ip not in ip_arr_22 :
+		    ck22_count += 1
+		    ip_arr_22[ip] = True
+
+		elif int(prefix[1]) > 52 and int(prefix[1]) <= 58 :
+		  ip = prefix64 >> 12
+		  if ip not in ip_arr_58 :
+		    ck58_count += 1
+		    ip_arr_58[ip] = True
+		  n58_cnt += 1 << (58 - int(prefix[1]))
+
+		  ip = prefix64 >> 18
+		  if ip not in ip_arr_52 :
+		    ck52_count += 1
+		    ip_arr_52[ip] = True
+
+		  ip = prefix64 >> 24
+		  if ip not in ip_arr_46 :
+		    ck46_count += 1
+		    ip_arr_46[ip] = True
+
+		  ip = prefix64 >> 30
+		  if ip not in ip_arr_40 :
+		    ck40_count += 1
+		    ip_arr_40[ip] = True
+
+		  ip = prefix64 >> 36
+		  if ip not in ip_arr_34 :
+		    ck34_count += 1
+		    ip_arr_34[ip] = True
+
+		  ip = prefix64 >> 42
+		  if ip not in ip_arr_28 :
+		    ck28_count += 1
+		    ip_arr_28[ip] = True
+
+		  ip = prefix64 >> 48
+		  if ip not in ip_arr_22 :
+		    ck22_count += 1
+		    ip_arr_22[ip] = True
+
+		elif int(prefix[1]) > 58 and int(prefix[1]) <= 64 :
+		  ip = prefix64 >> 6
 		  if ip not in ip_arr_64 :
 		    ck64_count += 1
 		    ip_arr_64[ip] = True
 		  n64_cnt += 1 << (64 - int(prefix[1]))
 
-		  ip = prefix64 >> 24
-		  if ip not in ip_arr_48 :
-		    ck48_count += 1
-		    ip_arr_48[ip] = True
+		  ip = prefix64 >> 12
+		  if ip not in ip_arr_58 :
+		    ck58_count += 1
+		    ip_arr_58[ip] = True
 
-		  ip = prefix64 >> 32
+		  ip = prefix64 >> 18
+		  if ip not in ip_arr_52 :
+		    ck52_count += 1
+		    ip_arr_52[ip] = True
+
+		  ip = prefix64 >> 24
+		  if ip not in ip_arr_46 :
+		    ck46_count += 1
+		    ip_arr_46[ip] = True
+
+		  ip = prefix64 >> 30
 		  if ip not in ip_arr_40 :
 		    ck40_count += 1
 		    ip_arr_40[ip] = True
 
+		  ip = prefix64 >> 36
+		  if ip not in ip_arr_34 :
+		    ck34_count += 1
+		    ip_arr_34[ip] = True
+
+		  ip = prefix64 >> 42
+		  if ip not in ip_arr_28 :
+		    ck28_count += 1
+		    ip_arr_28[ip] = True
+
 		  ip = prefix64 >> 48
-		  if ip not in ip_arr_32 :
-		    ck32_count += 1
-		    ip_arr_32[ip] = True
+		  if ip not in ip_arr_22 :
+		    ck22_count += 1
+		    ip_arr_22[ip] = True
 
 	    print "ck32_count=", ck32_count
 	    print "ck40_count=", ck40_count
@@ -111,5 +258,5 @@ for filename in filenames :
 	    print "total_n=", (n32_cnt + n40_cnt + n48_cnt + n64_cnt)/(1024*1024.0), " MB"
 	    
 	    #10240 byte is needed for B16 and C16, 0 byte is needed for N16
-	    ppc = (10240.0 + n32_cnt + n40_cnt + n48_cnt + n64_cnt + ck32_count * 20 * 1024 + + ck64_count * 10 * 1024 + (ck40_count + ck48_count) * 20 * 4)/(1024*1024)
-	    print filename + "  PPC=", ppc , "MB"
+	    poptrie = (65536 * 3 + n32_cnt + n40_cnt + n48_cnt + n64_cnt + ck32_count * 20 * 1024 + + ck64_count * 10 * 1024 + (ck40_count + ck48_count) * 20 * 4)/(1024*1024.0)
+	    print filename + "  PPC=", poptrie , "MB"
